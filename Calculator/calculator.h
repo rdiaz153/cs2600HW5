12b251e1e6ab46e37d4c94702d84bbf22c08d073
Ricardo Diaz <142759822+rdiaz153@users.noreply.github.com>
2026-03-03 15:59:17 -0800

    First stage TDD for multiply(). Added function prototype and created unit tests for it. Returned fail.

--- Calculator/calculator.h
+++ Calculator/calculator.h
@@ -5,4 +5,6 @@ int add(int a, int b);
 
 int subtract(int a, int b);
 
+int multiply(int a, int b);
+
 #endif
--- Calculator/test_calculator.c
+++ Calculator/test_calculator.c
@@ -71,6 +71,33 @@ void test_subtract_underflow(void) {
     TEST_ASSERT_TRUE(result > 0); // Checking if overflow occured
 }
 
+void test_multiply_positive_numbers(void) {
+    TEST_ASSERT_EQUAL(28, multiply(7, 4)); // Expecting 7 * 4 = 28
+}
+
+void test_multiply_positive_and_negative_numbers(void) {
+    TEST_ASSERT_EQUAL(-12, multiply(4, -3)); // Expecting 4 * -3 = -12
+}
+
+void test_multiply_negative_numbers(void) {
+    TEST_ASSERT_EQUAL(25, multiply(-5, -5)); // Expecting -5 * -5 = 25
+}
+
+void test_multiply_zero(void) {
+    TEST_ASSERT_EQUAL(0, multiply(10, 0)); // Expecting 10 * 0 = 0
+    TEST_ASSERT_EQUAL(0, multiply(0,0)); // Expecting 0 * 0 = 0
+}
+
+void test_multiply_overflow(void) {
+    int result = multiply(INT_MIN, 2);
+    TEST_ASSERT_TRUE(result > 0);
+}
+
+void test_multiply_underflow(void) {
+    int result = multiply(INT_MAX, 2);
+    TEST_ASSERT_TRUE(result < 0);
+}
+
 int main(void) {
     UNITY_BEGIN();
     RUN_TEST(test_add_positive_numbers);
@@ -86,5 +113,12 @@ int main(void) {
     RUN_TEST(test_subtract_zero);
     RUN_TEST(test_subtract_overflow);
     RUN_TEST(test_subtract_underflow);
+
+    RUN_TEST(test_multiply_positive_numbers);
+    RUN_TEST(test_multiply_positive_and_negative_numbers);
+    RUN_TEST(test_multiply_negative_numbers);
+    RUN_TEST(test_multiply_zero);
+    RUN_TEST(test_multiply_overflow);
+    RUN_TEST(test_multiply_underflow);
     return UNITY_END();
 }
