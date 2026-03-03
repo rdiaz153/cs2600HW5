edd25727265912fc4f8348f13c6e0b685dd2a6a1
Ricardo Diaz <142759822+rdiaz153@users.noreply.github.com>
2026-03-03 15:00:56 -0800

    First stage of TDD for subtract(). Added function prototype and unit tests for subtract().

--- Calculator/test_calculator.c
+++ Calculator/test_calculator.c
@@ -44,6 +44,33 @@ void test_add_underflow(void) {
     TEST_ASSERT_TRUE(result > 0); // This checks if underflow occured
 }
 
+void test_subtract_positive_numbers(void) {
+    TEST_ASSERT_EQUAL(2, subtract(5, 3)); // Expecting 5 - 3 = 2
+}
+
+void test_subtract_positive_and_negative_numbers(void) {
+    TEST_ASSERT_EQUAL(6, subtract(5, -1)); // Expecting 5 - (-1) = 6
+}
+
+void test_subtract_negative_numbers(void) {
+    TEST_ASSERT_EQUAL(-1, subtract(-4, -3)); // Expecting -4 - (-3) = -1
+}
+
+void test_subtract_zero(void) {
+    TEST_ASSERT_EQUAL(5, subtract(5, 0)); // Expecting 5 - 0 = 5
+    TEST_ASSERT_EQUAL(0, subtract(0, 0)); // Expecting 0 - 0 = 0
+}
+
+void test_subtract_overflow(void) {
+    int result = subtract(INT_MAX, -1); 
+    TEST_ASSERT_TRUE(result < 0); // Checking if overflow occured
+}
+
+void test_subtract_underflow(void) {
+    int result = subtract(INT_MIN, 1);
+    TEST_ASSERT_TRUE(result > 0); // Checking if overflow occured
+}
+
 int main(void) {
     UNITY_BEGIN();
     RUN_TEST(test_add_positive_numbers);
@@ -52,5 +79,12 @@ int main(void) {
     RUN_TEST(test_add_zero);
     RUN_TEST(test_add_overflow);
     RUN_TEST(test_add_underflow);
+
+    RUN_TEST(test_subtract_positive_numbers);
+    RUN_TEST(test_subtract_positive_and_negative_numbers);
+    RUN_TEST(test_subtract_negative_numbers);
+    RUN_TEST(test_subtract_zero);
+    RUN_TEST(test_subtract_overflow);
+    RUN_TEST(test_subtract_underflow);
     return UNITY_END();
 }
