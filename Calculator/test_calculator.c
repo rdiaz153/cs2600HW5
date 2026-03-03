851332143cc65991f67d546256b76dc65920c2c2
Ricardo Diaz <142759822+rdiaz153@users.noreply.github.com>
2026-03-02 23:32:16 -0800

    Added unit tests for adding overflow and underflow.

--- Calculator/test_calculator.c
+++ Calculator/test_calculator.c
@@ -32,11 +32,25 @@ void test_add_zero(void) {
     TEST_ASSERT_EQUAL(0, add(0,0)); // We expect 0 + 0 = 0
 }
 
+// Test integer overflow
+void test_add_overflow(void) {
+    int result = add(INT_MAX, 1);
+    TEST_ASSERT_TRUE(result < 0); // This checks if overflow occured
+}
+
+// Test integer underflow
+void test_add_underflow(void) {
+    int result = add(INT_MIN, -1);
+    TEST_ASSERT_TRUE(result > 0); // This checks if underflow occured
+}
+
 int main(void) {
     UNITY_BEGIN();
     RUN_TEST(test_add_positive_numbers);
     RUN_TEST(test_add_positive_and_negative_numbers);
     RUN_TEST(test_add_negative_numbers);
     RUN_TEST(test_add_zero);
+    RUN_TEST(test_add_overflow);
+    RUN_TEST(test_add_underflow);
     return UNITY_END();
 }
